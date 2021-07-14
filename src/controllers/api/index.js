3a030ee4ff4471d81c2153af81c6523a4f549c89
Michael Crates <michaelcrates89@hotmail.co.uk>
2021-07-14 21:19:03 +0100

    added api routes and controllers

---
+++ src/controllers/api/index.js
@@ -0,0 +1,16 @@
+const { model } = require("mongoose");
+
+const getWorkouts = (req, res) => {};
+
+const updateWorkout = (req, res) => {};
+
+const createWorkout = (req, res) => {};
+
+const getAggregatedWorkout = (req, res) => {};
+
+module.exports = {
+  getWorkouts,
+  updateWorkout,
+  createWorkout,
+  getAggregatedWorkout,
+};
