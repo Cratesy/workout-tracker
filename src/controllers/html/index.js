f358ca24e6fc019cc8d3012554bf232581fba793
Michael Crates <michaelcrates89@hotmail.co.uk>
2021-07-14 21:43:24 +0100

    update paths

--- src/controllers/html/index.js
+++ src/controllers/html/index.js
@@ -2,15 +2,15 @@ const { model } = require("mongoose");
 const path = require("path");
 
 const renderStats = (req, res) => {
-  res.sendFile(path.join(__dirname, "../../../public/stats.html"));
+  res.sendFile(path.join(__dirname, "../../public/stats.html"));
 };
 
 const renderExercise = (req, res) => {
-  res.sendFile(path.join(__dirname, "../../../public/exercise.html"));
+  res.sendFile(path.join(__dirname, "../../public/exercise.html"));
 };
 
 const renderHome = (req, res) => {
-  res.sendFile(path.join(__dirname, "../../../public/index.html"));
+  res.sendFile(path.join(__dirname, "../../public/index.html"));
 };
 
 module.exports = {
