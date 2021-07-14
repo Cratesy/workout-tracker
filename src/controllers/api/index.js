09da5a82458df9db5eaa63bcb5e9953a41875c09
Michael Crates <michaelcrates89@hotmail.co.uk>
2021-07-14 21:36:38 +0100

    move files

--- src/controllers/api/index.js
+++ src/controllers/api/index.js
@@ -1,10 +1,19 @@
 const { model } = require("mongoose");
 
+const { Workout } = require("../../models");
+
 const getWorkouts = (req, res) => {};
 
 const updateWorkout = (req, res) => {};
 
-const createWorkout = (req, res) => {};
+const createWorkout = (req, res) => {
+  try {
+    const Workout = await workout.create({});
+    return res.json(Workout);
+  } catch (error) {
+    return res.status(500).json({ error: "failed to create workout" });
+  }
+};
 
 const getAggregatedWorkout = (req, res) => {};
 
