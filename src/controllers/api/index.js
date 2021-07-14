943bb15d414e67850cedb828403d6058f0acbfc3
Michael Crates <michaelcrates89@hotmail.co.uk>
2021-07-14 22:27:09 +0100

    added update to workout /db name

--- src/controllers/api/index.js
+++ src/controllers/api/index.js
@@ -4,7 +4,22 @@ const { Workout } = require("../../models");
 
 const getWorkouts = (req, res) => {};
 
-const updateWorkout = (req, res) => {};
+const updateWorkout = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const exercise = req.body;
+
+    const updateWorkout = await Workout.findByIdAndUpdate(
+      id,
+      { $push: { exercises: exercise } },
+      { new: true, runValidators: true }
+    );
+    return res.json(updateWorkout);
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).json({ error: "failed to update workout" });
+  }
+};
 
 const createWorkout = async (req, res) => {
   try {
